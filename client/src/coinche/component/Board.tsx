d84295ffab64214f58103e36f92bdcbea7c18891
Oliboy50 <oliver.thebault@gmail.com>
2020-01-26 21:45:58 +0100

    feat(client): add PlayedCards component

--- client/src/coinche/component/Board.tsx
+++ client/src/coinche/component/Board.tsx
@@ -4,7 +4,6 @@ import styles from './Board.module.css';
 import {
   GameStatePlayerView,
   getPlayerPartner,
-  getTurnOrder,
   Moves,
   PhaseID,
   PlayerID,
@@ -15,22 +14,8 @@ import {OtherPlayerCardsComponent} from './OtherPlayerCards';
 import {PlayerSaidComponent} from './PlayerSaid';
 import {PlayerTurnIndicatorComponent} from './PlayerTurnIndicator';
 import {PreviousCardsPlayedMenuComponent} from './PreviousCardsPlayedMenu';
-
-const getPlayerIDForPosition = (bottomPlayerID: PlayerID, position: 'top' | 'left' | 'right' | 'bottom'): PlayerID => {
-  if (position === 'bottom') {
-    return bottomPlayerID;
-  }
-
-  const turnOrder = getTurnOrder(bottomPlayerID);
-  switch (position) {
-    case 'right':
-      return turnOrder[1];
-    case 'top':
-      return turnOrder[2];
-    case 'left':
-      return turnOrder[3];
-  }
-};
+import {getPlayerIDForPosition} from '../service/getPlayerIDForPosition';
+import {PlayedCardsComponent} from './PlayedCards';
 
 export const BoardComponent: React.FunctionComponent<BoardProps<GameStatePlayerView, Moves, PlayerID, PhaseID>> = ({
   G,
@@ -57,60 +42,70 @@ export const BoardComponent: React.FunctionComponent<BoardProps<GameStatePlayerV
   // @TODO use isDisplayedPreviousCardsPlayed to display previous cards played (when we'll be able to show the current cards played, it will replace the current cards by the previous... maybe)
   const [isDisplayedPreviousCardsPlayed, setIsDisplayedPreviousCardsPlayed] = useState(false);
 
+  const playedCards = isDisplayedPreviousCardsPlayed ? G.playersCardsPlayedInPreviousTurn : G.playersCardsPlayedInCurrentTurn;
+
   return (
-    <React.Fragment>
-      <div className={styles.board}>
-        <div className={`${styles.player} ${styles.top}`}>
-          {currentPhaseNeedsPlayerMove && currentPlayerIsTopPlayer && (
-            <PlayerTurnIndicatorComponent />
-          )}
-          {currentPhaseIsTalk && !currentPlayerIsTopPlayer && G.playersSaid[topPlayerID] && (
-            <PlayerSaidComponent playerSaid={G.playersSaid[topPlayerID]}/>
-          )}
-          <OtherPlayerCardsComponent cards={G.playersCards[topPlayerID]} />
-        </div>
-        <div className={`${styles.player} ${styles.left}`}>
-          {currentPhaseNeedsPlayerMove && currentPlayerIsLeftPlayer && (
-            <PlayerTurnIndicatorComponent />
-          )}
-          {currentPhaseIsTalk && !currentPlayerIsLeftPlayer && G.playersSaid[leftPlayerID] && (
-            <PlayerSaidComponent playerSaid={G.playersSaid[leftPlayerID]}/>
-          )}
-          <OtherPlayerCardsComponent cards={G.playersCards[leftPlayerID]} />
-        </div>
-        <div className={`${styles.player} ${styles.right}`}>
-          {currentPhaseNeedsPlayerMove && currentPlayerIsRightPlayer && (
-            <PlayerTurnIndicatorComponent />
-          )}
-          {currentPhaseIsTalk && !currentPlayerIsRightPlayer && G.playersSaid[rightPlayerID] && (
-            <PlayerSaidComponent playerSaid={G.playersSaid[rightPlayerID]}/>
-          )}
-          <OtherPlayerCardsComponent cards={G.playersCards[rightPlayerID]} />
-        </div>
-        <div className={`${styles.player} ${styles.bottom}`}>
-          {currentPhaseNeedsPlayerMove && currentPlayerIsBottomPlayer && (
-            <PlayerTurnIndicatorComponent />
-          )}
-          {currentPhaseIsTalk && !currentPlayerIsBottomPlayer && G.playersSaid[bottomPlayerID] && (
-            <PlayerSaidComponent playerSaid={G.playersSaid[bottomPlayerID]}/>
-          )}
-          {currentPhaseIsTalk && currentPlayerIsBottomPlayer && (
-            <TalkMenuComponent moves={moves} playersSaid={G.playersSaid} />
-          )}
-          {currentPhaseIsPlayCards && !isFirstPlayCardTurn && (
-            <PreviousCardsPlayedMenuComponent isDisplayedPreviousCardsPlayed={isDisplayedPreviousCardsPlayed} toggleIsDisplayedPreviousCardsPlayed={() => setIsDisplayedPreviousCardsPlayed(!isDisplayedPreviousCardsPlayed)} />
-          )}
-          <MyCardsComponent
-            cards={G.playerCards}
-            isMyTurnToPlayACard={currentPhaseIsPlayCards && currentPlayerIsBottomPlayer}
-            playCard={moves.playCard}
-            trumpMode={G.trumpMode}
-            playersCardsPlayedInCurrentTurn={G.playersCardsPlayedInCurrentTurn}
-            firstPlayerInCurrentTurn={G.firstPlayerInCurrentTurn}
-            playerPartner={getPlayerPartner(playerID)}
-          />
-        </div>
+    <div className={styles.board}>
+      {/* Played cards area */}
+      <div className={styles.playedCardsArea}>
+        <PlayedCardsComponent playerID={playerID} playedCards={playedCards} />
+        {currentPhaseIsPlayCards && !isFirstPlayCardTurn && (
+          <PreviousCardsPlayedMenuComponent isDisplayedPreviousCardsPlayed={isDisplayedPreviousCardsPlayed} toggleIsDisplayedPreviousCardsPlayed={() => setIsDisplayedPreviousCardsPlayed(!isDisplayedPreviousCardsPlayed)} />
+        )}
       </div>
-    </React.Fragment>
+
+      {/* Players */}
+      {!isDisplayedPreviousCardsPlayed && (
+        <React.Fragment>
+          <div className={`${styles.player} ${styles.top}`}>
+            {currentPhaseNeedsPlayerMove && currentPlayerIsTopPlayer && (
+              <PlayerTurnIndicatorComponent />
+            )}
+            {currentPhaseIsTalk && !currentPlayerIsTopPlayer && G.playersSaid[topPlayerID] && (
+              <PlayerSaidComponent playerSaid={G.playersSaid[topPlayerID]}/>
+            )}
+            <OtherPlayerCardsComponent cards={G.playersCards[topPlayerID]} />
+          </div>
+          <div className={`${styles.player} ${styles.left}`}>
+            {currentPhaseNeedsPlayerMove && currentPlayerIsLeftPlayer && (
+              <PlayerTurnIndicatorComponent />
+            )}
+            {currentPhaseIsTalk && !currentPlayerIsLeftPlayer && G.playersSaid[leftPlayerID] && (
+              <PlayerSaidComponent playerSaid={G.playersSaid[leftPlayerID]}/>
+            )}
+            <OtherPlayerCardsComponent cards={G.playersCards[leftPlayerID]} />
+          </div>
+          <div className={`${styles.player} ${styles.right}`}>
+            {currentPhaseNeedsPlayerMove && currentPlayerIsRightPlayer && (
+              <PlayerTurnIndicatorComponent />
+            )}
+            {currentPhaseIsTalk && !currentPlayerIsRightPlayer && G.playersSaid[rightPlayerID] && (
+              <PlayerSaidComponent playerSaid={G.playersSaid[rightPlayerID]}/>
+            )}
+            <OtherPlayerCardsComponent cards={G.playersCards[rightPlayerID]} />
+          </div>
+          <div className={`${styles.player} ${styles.bottom}`}>
+            {currentPhaseNeedsPlayerMove && currentPlayerIsBottomPlayer && (
+              <PlayerTurnIndicatorComponent />
+            )}
+            {currentPhaseIsTalk && !currentPlayerIsBottomPlayer && G.playersSaid[bottomPlayerID] && (
+              <PlayerSaidComponent playerSaid={G.playersSaid[bottomPlayerID]}/>
+            )}
+            {currentPhaseIsTalk && currentPlayerIsBottomPlayer && (
+              <TalkMenuComponent moves={moves} playersSaid={G.playersSaid} />
+            )}
+            <MyCardsComponent
+              cards={G.playerCards}
+              isMyTurnToPlayACard={currentPhaseIsPlayCards && currentPlayerIsBottomPlayer}
+              playCard={moves.playCard}
+              trumpMode={G.trumpMode}
+              playersCardsPlayedInCurrentTurn={G.playersCardsPlayedInCurrentTurn}
+              firstPlayerInCurrentTurn={G.firstPlayerInCurrentTurn}
+              playerPartner={getPlayerPartner(playerID)}
+            />
+          </div>
+        </React.Fragment>
+      )}
+    </div>
   );
 };
