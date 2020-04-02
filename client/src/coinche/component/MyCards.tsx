804ce20c77abff07d15310462cdec5b08b1df387
Oliboy50 <oliver.thebault@gmail.com>
2020-04-03 00:02:48 +0200

    feat(client): fixes #38

--- client/src/coinche/component/MyCards.tsx
+++ client/src/coinche/component/MyCards.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {
   Card,
-  CardColor,
-  CardName,
   GameState,
   isPlayableCard,
   isSameCard,
@@ -10,92 +8,8 @@ import {
   PlayerID,
   TrumpMode,
 } from '../../shared/coinche';
+import {sortCards} from '../service/sortCards';
 import {CardComponent} from './Card';
-
-const sortCardsFromSpadeToHeartAndFromAceToSeven = (a: Card, b: Card): number => {
-  // sort by color
-  if (a.color === CardColor.Spade && b.color !== CardColor.Spade) {
-    return -1;
-  }
-  if (a.color === CardColor.Diamond) {
-    switch (b.color) {
-      case CardColor.Heart:
-      case CardColor.Club:
-        return -1;
-      case CardColor.Spade:
-        return 1;
-    }
-  }
-  if (a.color === CardColor.Club) {
-    switch (b.color) {
-      case CardColor.Heart:
-        return -1;
-      case CardColor.Spade:
-      case CardColor.Diamond:
-        return 1;
-    }
-  }
-  if (a.color === CardColor.Heart && b.color !== CardColor.Heart) {
-    return 1;
-  }
-  // both cards have the same color
-
-  // sort by name
-  switch (a.name) {
-    case CardName.Ace:
-      return -1;
-    case CardName.King:
-      switch (b.name) {
-        case CardName.Ace:
-          return 1;
-        default:
-          return -1;
-      }
-    case CardName.Queen:
-      switch (b.name) {
-        case CardName.Ace:
-        case CardName.King:
-          return 1;
-        default:
-          return -1;
-      }
-    case CardName.Jack:
-      switch (b.name) {
-        case CardName.Ace:
-        case CardName.King:
-        case CardName.Queen:
-          return 1;
-        default:
-          return -1;
-      }
-    case CardName.Ten:
-      switch (b.name) {
-        case CardName.Nine:
-        case CardName.Eight:
-        case CardName.Seven:
-          return -1;
-        default:
-          return 1;
-      }
-    case CardName.Nine:
-      switch (b.name) {
-        case CardName.Eight:
-        case CardName.Seven:
-          return -1;
-        default:
-          return 1;
-      }
-    case CardName.Eight:
-      switch (b.name) {
-        case CardName.Seven:
-          return -1;
-        default:
-          return 1;
-      }
-    case CardName.Seven:
-      return 1;
-  }
-};
 
 type ComponentProps = {
   cards: Card[],
@@ -121,7 +35,7 @@ export const MyCardsComponent: React.FunctionComponent<ComponentProps> = ({
 }) => {
   return (
     <div className="myCards">
-      {cards.sort(sortCardsFromSpadeToHeartAndFromAceToSeven).map(card => {
+      {sortCards(cards).map(card => {
         const key = `${card.color}${card.name}`;
         const isBelotCard = belotCards.some(bc => isSameCard(bc, card));
         const playCardState = (isMyTurnToPlayACard && trumpMode)
